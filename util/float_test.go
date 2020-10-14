ae26e9ac075494b182747c190ba27a3353e6f262
Jim <jmwri93@gmail.com>
2020-10-14 09:56:05 +0100

    Add util tests

---
+++ util/float_test.go
@@ -0,0 +1,99 @@
+package util_test
+
+import (
+	"fmt"
+	"math/rand"
+	"neatgo/util"
+	"testing"
+)
+
+func TestRandFloat(t *testing.T) {
+	rand.Seed(12345)
+	tests := []struct {
+		min      float64
+		max      float64
+		expected float64
+	}{
+		{min: 0, max: 1, expected: .8487305991992138},
+		{min: 0, max: 1, expected: .6451080292174168},
+		{min: 0, max: 1, expected: .7382079884862905},
+		{min: 0, max: 1, expected: .31522206779732853},
+		{min: 0, max: 1, expected: .057001989921077224},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%f - %f", testCase.min, testCase.max), func(t *testing.T) {
+			res := util.RandFloat(testCase.min, testCase.max)
+			if res != testCase.expected {
+				t.Errorf("expected %f, got %f", testCase.expected, res)
+			}
+		})
+	}
+}
+
+func TestReduceFloat(t *testing.T) {
+	tests := []struct {
+		init     float64
+		sequence []float64
+		expected float64
+	}{
+		{0, []float64{2, 3, 2, 4, 5}, 16},
+		{5, []float64{2, 3, 2, 4, 5}, 21},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%f, %v", testCase.init, testCase.sequence), func(t *testing.T) {
+			res := util.ReduceFloat(util.SumFloat, testCase.sequence, testCase.init)
+
+			if res != testCase.expected {
+				t.Errorf("expected %f, got %f", testCase.expected, res)
+			}
+		})
+	}
+}
+
+func TestMultiplyFloat(t *testing.T) {
+	tests := []struct {
+		a        float64
+		b        float64
+		expected float64
+	}{
+		{5, 3, 15},
+		{2, 1, 2},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v * %v", testCase.a, testCase.b), func(t *testing.T) {
+			res := util.MultiplyFloat(testCase.a, testCase.b)
+
+			if res != testCase.expected {
+				t.Errorf("expected %f, got %f", testCase.expected, res)
+			}
+		})
+	}
+}
+
+func TestSumFloat(t *testing.T) {
+	tests := []struct {
+		a        float64
+		b        float64
+		expected float64
+	}{
+		{5, 3, 8},
+		{2, 1, 3},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v + %v", testCase.a, testCase.b), func(t *testing.T) {
+			res := util.SumFloat(testCase.a, testCase.b)
+
+			if res != testCase.expected {
+				t.Errorf("expected %f, got %f", testCase.expected, res)
+			}
+		})
+	}
+}
---
+++ util/slice_test.go
@@ -0,0 +1,92 @@
+package util_test
+
+import (
+	"fmt"
+	"neatgo/util"
+	"testing"
+)
+
+func TestSliceOfFloatEqual(t *testing.T) {
+	tests := []struct {
+		a        []float64
+		b        []float64
+		expected bool
+	}{
+		{a: []float64{1, 2, 3, 4, 5}, b: []float64{1, 2, 3, 4, 5}, expected: true},
+		{a: []float64{2, 1, 3, 4, 5}, b: []float64{1, 2, 3, 4, 5}, expected: false},
+	}
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v == %v", testCase.a, testCase.b), func(t *testing.T) {
+			res := util.SliceOfFloatEqual(testCase.a, testCase.b)
+
+			if res != testCase.expected {
+				t.Errorf("expected %v, got %v", testCase.expected, res)
+			}
+		})
+	}
+}
+
+func TestSliceOfInt64Equal(t *testing.T) {
+	tests := []struct {
+		a        []int64
+		b        []int64
+		expected bool
+	}{
+		{a: []int64{1, 2, 3, 4, 5}, b: []int64{1, 2, 3, 4, 5}, expected: true},
+		{a: []int64{2, 1, 3, 4, 5}, b: []int64{1, 2, 3, 4, 5}, expected: false},
+	}
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v == %v", testCase.a, testCase.b), func(t *testing.T) {
+			res := util.SliceOfInt64Equal(testCase.a, testCase.b)
+
+			if res != testCase.expected {
+				t.Errorf("expected %v, got %v", testCase.expected, res)
+			}
+		})
+	}
+}
+
+func TestSortSliceOfFloatAsc(t *testing.T) {
+	tests := []struct {
+		input    []float64
+		expected []float64
+	}{
+		{input: []float64{4, 2, 5, 1, 3}, expected: []float64{1, 2, 3, 4, 5}},
+		{input: []float64{4, 3, 4, 1, 5, 2, 1.2, 1.3}, expected: []float64{1, 1.2, 1.3, 2, 3, 4, 4, 5}},
+	}
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v", testCase.input), func(t *testing.T) {
+			util.SortSliceOfFloatAsc(testCase.input)
+			res := util.SliceOfFloatEqual(testCase.input, testCase.expected)
+
+			if res != true {
+				t.Errorf("expected %v, got %v", testCase.expected, testCase.input)
+			}
+		})
+	}
+}
+
+func TestRemoveInt64FromSlice(t *testing.T) {
+	tests := []struct {
+		input    []int64
+		remove   int64
+		expected []int64
+	}{
+		{input: []int64{4, 2, 5, 1, 3}, remove: 5, expected: []int64{4, 2, 1, 3}},
+		{input: []int64{1, 2, 2, 2, 3}, remove: 2, expected: []int64{1, 2, 2, 3}},
+		{input: []int64{1, 2, 2, 2, 3}, remove: 5, expected: []int64{1, 2, 2, 2, 3}},
+	}
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(fmt.Sprintf("%v - %d", testCase.input, testCase.remove), func(t *testing.T) {
+			res := util.RemoveInt64FromSlice(testCase.input, testCase.remove)
+
+			if !util.SliceOfInt64Equal(res, testCase.expected) {
+				t.Errorf("expected %v, got %v", testCase.expected, res)
+			}
+		})
+	}
+}
